debe6e104c50e4e92feceff5503d2544ed3ba80f
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-03-23 17:26:56 -0400

    delete useless code

--- src/cacheImages.ts
+++ src/cacheImages.ts
@@ -1,31 +1,18 @@
+// establish type for function that returns a promise that resolves to an img.onload event or rejects with an img.onerror event.
+
 /**
  * cacheImages takes in an array of images
  * to cache prior to using them.
  */
-// async function cacheImages(srcArray: Array<string>) {
-//   const promises = srcArray.map((src: string) => {
-//     return new Promise((resolve, reject) => {
-//       const img = new Image();
 
-//       img.src = src;
-//       // on img load, resolve promise.
-//       img.onload = resolve(src);
-//       // on error, reject promise.
-//       img.onerror = reject();
-//     });
-//   });
-//   await Promise.all(promises);
-//   const results = await Promise.allSettled(promises);
-//   results.forEach(result => console.log(result.status));
-// };
-function cacheImages( images : Array<string>): Promise<Array<unknown>> {
+function cacheImages( images : Array<string>): Promise<unknown> {
   return Promise.all(
     images.map((image: string) => {
       return new Promise((resolve, reject) => {
         const img: HTMLImageElement = new Image();
         img.src = image;
-        img.onload = resolve(image);
-        img.onerror = reject();
+        img.onload = () => resolve(image);
+        img.onerror = () => reject();
       });
     })
   );
